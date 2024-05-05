07cda90dca463f2583df06e839e022cd99fc3890
phanatagama <phanatagama@gmail.com>
2024-05-05 19:06:26 +0700

    feat: change base url

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -7,4 +7,6 @@ export const showLogger = isLocal
 
 export const apiBaseUrl = 'https://forum-api.dicoding.dev/v1';
 
-export const localhostUrl = 'http://localhost:3000/api/v1';
+export const localhostUrl = isLocal
+  ? 'http://localhost:3000/api/v1'
+  : 'https://tanya.vercel.app/api/v1';

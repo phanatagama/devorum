724e75ac2db7723c62a1317c6a39cf10a0196088
phanatagama <phanatagama@gmail.com>
2024-05-06 01:54:58 +0700

    feat: change env key

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -1,12 +1,12 @@
 export const isProd = process.env.NODE_ENV === 'production';
 export const isLocal =
   process.env.NODE_ENV === 'development' ||
-  process.env.PROJECT_URL === undefined ||
-  process.env.PROJECT_URL === 'undefined';
+  process.env.NEXT_PUBLIC_PROJECT_URL === undefined ||
+  process.env.NEXT_PUBLIC_PROJECT_URL === 'undefined';
 const vercelUrl =
-  process.env.PROJECT_URL === undefined
+  process.env.NEXT_PUBLIC_PROJECT_URL === undefined
     ? 'devorum-git-testing-phanatagamas-projects'
-    : process.env.PROJECT_URL;
+    : process.env.NEXT_PUBLIC_PROJECT_URL;
 export const showLogger = isLocal
   ? true
   : process.env.NEXT_PUBLIC_SHOW_LOGGER === 'true' ?? false;

a433163fa3efd14af332c86ff947b2a31db6d79c
Restyled.io <commits@restyled.io>
2024-05-05 17:03:41 +0000

    Restyled by clang-format

--- next.config.js
+++ next.config.js
@@ -1,57 +1,56 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    dirs: ['src'],
+  eslint : {
+    dirs : [ 'src' ],
   },
 
-  reactStrictMode: true,
-  swcMinify: true,
+  reactStrictMode : true,
+  swcMinify : true,
 
   // Uncoment to add domain whitelist
-  images: {
-    dangerouslyAllowSVG: true,
-    domains: ['res.cloudinary.com', 'ui-avatars.com', 'localhost'],
-    remotePatterns: [
+  images : {
+    dangerouslyAllowSVG : true,
+    domains : [ 'res.cloudinary.com', 'ui-avatars.com', 'localhost' ],
+    remotePatterns : [
       {
-        protocol: 'https',
-        hostname: 'ui-avatars.com',
-        port: '',
+        protocol : 'https',
+        hostname : 'ui-avatars.com',
+        port : '',
       },
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
+        protocol : 'http',
+        hostname : 'localhost',
+        port : '3000',
       },
     ],
   },
 
   webpack(config) {
     // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.('.svg')
-    );
+    const fileLoaderRule =
+        config.module.rules.find((rule) => rule.test?.test?.('.svg'));
 
     config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      // Convert all other *.svg imports to React components
-      {
-        test: /\.svg$/i,
-        issuer: { not: /\.(css|scss|sass)$/ },
-        resourceQuery: { not: /url/ }, // exclude if *.svg?url
-        loader: '@svgr/webpack',
-        options: {
-          dimensions: false,
-          titleProp: true,
+        // Reapply the existing rule, but only for svg imports ending in ?url
+        {
+          ...fileLoaderRule,
+          test : /\.svg$/i,
+          resourceQuery : /url/, // *.svg?url
         },
-      }
-    );
+        // Convert all other *.svg imports to React components
+        {
+          test : /\.svg$/i,
+          issuer : {not : /\.(css|scss|sass)$/},
+          resourceQuery : {not : /url/}, // exclude if *.svg?url
+          loader : '@svgr/webpack',
+          options : {
+            dimensions : false,
+            titleProp : true,
+          },
+        });
 
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    // Modify the file loader rule to ignore *.svg, since we have it handled
+    // now.
     fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
@@ -60,18 +59,18 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: '/api/v1/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+        source : '/api/v1/:path*',
+        headers : [
+          {key : 'Access-Control-Allow-Credentials', value : 'true'},
+          {key : 'Access-Control-Allow-Origin', value : '*'},
           {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            key : 'Access-Control-Allow-Methods',
+            value : 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
           },
           {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+            key : 'Access-Control-Allow-Headers',
+            value :
+                'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
           },
         ],
       },
